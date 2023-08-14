9c27d6f27df1b7fec5a3d73cf59d899051e95e81
Pavel 'Strajk' Dolecek <strajk@me.com>
2023-08-14 13:22:15 +0200

    Download also API specs, update Readme

--- README.md
+++ README.md
@@ -1,18 +1,103 @@
-## All OpenAI Plugin manifests, in git, updated daily 
+# All OpenAI Plugin manifests, in git, updated daily
+
+Fetch OpenAI Plugin manifests and their API spec, store in git in nice format, repeat every day.
+
+🥷 *Note that only API definitions which are not behind authentication are fetched. But vast majority of them are public.*
+
+💾 *Plugins are identified by unique randomly generated UUIDs, which aren't practical for human use. I decided to use combination of the plugin's domain and name for the filename where the plugin's data is stored.* 
 
 Stored as:
 
-- array in [resultsArray.json](./resultsArray.json)
-- object indexed by plugin name in [resultsIndexed.json](./resultsIndexed.json)
-- separate file per plugin name in [results/](./results) folder
-  - 💡TIP: Press `t` to open file finder on GitHub   
+- array in [resultsArray.json](./resultsArray.json) (only plugin manifests)
+- object indexed by plugin name in [resultsIndexed.json](./resultsIndexed.json) (only plugin manifests)
+- separate file per plugin name in [results/](./results) folder (both plugin manifest and API spec)
+  - 💡TIP: Press `t` to open file finder on GitHub
 
-#### Why
+Examples:
+
+- Kiwi.com [Plugin](./results/chatgpt-plugin_kiwi_com__kiwicom_assistant_and_flights_search.json), [API](./results/chatgpt-plugin_kiwi_com__kiwicom_assistant_and_flights_search.api.json)
+- Ai PDF [Plugin](./results/plugin-3c56b9d4c8a6465998395f28b6a445b2-jexkai4vea-uc_a_run_app__Ai_PDF.json), [API](./results/plugin-3c56b9d4c8a6465998395f28b6a445b2-jexkai4vea-uc_a_run_app__Ai_PDF.api.yaml)
+- Wikipedia [Plugin](./results/chatgptplugin_enterprise_wikimedia_com__wikipedia.json), [API](./results/chatgptplugin_enterprise_wikimedia_com__wikipedia.api.json)
+- Wolfram [Plugin](./results/wolframalpha_com__Wolfram.json), [API](./results/wolframalpha_com__Wolfram.api.json)
+- Zapier [Plugin](./results/zapier_com__Zapier.json), [API](./results/zapier_com__Zapier.api.json)
+ 
+### Why
 
 - to see how the plugin ecosystem evolves over time
 - to discover newly created plugins – types, categories, and authors
 - to analyze model prompts patterns & best practices
 - to have it tracked by good old git 🤓
+
+### Example
+
+#### Example of Plugin definition
+
+```json
+{
+  "id": "plugin-abc12345-7f59-4bc9-9f3e-36f5ca12d8ef",
+  "domain": "chatgpt.example.com",
+  "namespace": "foo_calculator",
+  "status": "approved",
+  "manifest": {
+    "schema_version": "v1",
+    "name_for_model": "foo_calculator",
+    "name_for_human": "Foo Calculator",
+    "description_for_model": "THE REALLY INTERESTING PART!",
+    "description_for_human": "",
+    "auth": { ... },
+    "api": {
+      "type": "openapi",
+      "url": "https://chatgpt.example.com/openapi.json"
+    },
+    "logo_url": "https://chatgpt.example.com/logo.png",
+    "contact_email": "chatgpt@foo.com",
+    "legal_info_url": "https://chatgpt.example.com/legal",
+  },
+  "oauth_client_id": null,
+  "categories": [
+    { "id": "newly_added", "title": "New" }
+  ]
+}
+```
+
+#### Example of Plugin API definition
+
+```yaml
+info:
+  title: Calculator Chat
+  description: A plugin that allows users to do math in chat.
+  version: 1.0.0
+servers:
+  - url: https://chatgpt.example.com
+paths:
+  /calculate:
+    post:
+      summary: Calculate a math expression
+      operationId: calculate
+      description: ...
+      requestBody:
+        required: true
+        content:
+          application/json:
+            schema:
+              type: object
+              properties:
+                expression:
+                  type: string
+                  description: The math expression to calculate
+                  example: 2 + 2
+      responses:
+        200:
+          description: The result of the calculation
+          content:
+            application/json:
+              schema:
+                type: object
+                properties:
+                  result:
+                    type: number
+                    description: The result of the calculation
+```
 
 #### Resources
 
--- scrape.mjs
+++ scrape.mjs
@@ -2,61 +2,85 @@ import * as fs from 'node:fs'
 
 if (!process.env.OPENAI_TOKEN) throw new Error(`🚨 OPENAI_TOKEN env var is required`)
 
-const now = new Date().toISOString() // static for the whole run
-const limit = 32 // default is 8, 32 works fine, more is maybe possible but let's be nice
-let offset = 0
-let total = null // set after first request
+const startedAt = new Date().toISOString() // static for the whole run
+let results = []
 
-const results = []
-async function scrape () {
-  const res = await fetch(`https://chat.openai.com/backend-api/aip/p/approved?offset=${offset}&limit=${limit}&search=`, {
-    headers: {
-      authorization: `Bearer ${process.env.OPENAI_TOKEN}`,
-      "cache-control": `no-cache`,
-      "content-type": `application/json`,
-    },
-    referrer: `https://chat.openai.com/`,
-  })
-  const data = await res.json()
-  const { count, items } = data
-  for (const item of items) {
-    item.__scrapedAt = now // maybe will be useful later
-    delete item.user_settings // related to the authed user, not the plugin
-  } 
+async function downloadPluginManifests() {
+  const limit = 32 // default is 8, 32 works fine, more is maybe possible but let's be nice
+  let offset = 0
+  let total = null // set after first request
 
-  if (total && count < total) console.log(`🧐 Suspicious, count got smaller: ${total} » ${count}`)
-  total = count
-  offset += limit
-  results.push(...items)
-  console.log(`⛏️ Scraped ${results.length}/${count}`)
-  await new Promise(resolve => setTimeout(resolve, 500)) // wait to not overload the API
+  async function scrape () {
+    const res = await fetch(`https://chat.openai.com/backend-api/aip/p/approved?offset=${offset}&limit=${limit}&search=`, {
+      headers: {
+        authorization: `Bearer ${process.env.OPENAI_TOKEN}`,
+        "cache-control": `no-cache`,
+        "content-type": `application/json`,
+      },
+      referrer: `https://chat.openai.com/`,
+    })
+    const data = await res.json()
+    const { count, items } = data
+    for (const item of items) {
+      item.__scrapedAt = startedAt // maybe will be useful later
+      delete item.user_settings // related to the authed user, not the plugin
+    }
+
+    if (total && count < total) console.log(`🧐 Suspicious, count got smaller: ${total} » ${count}`)
+    total = count
+    offset += limit
+    results.push(...items)
+    console.log(`⛏️ Scraped ${results.length}/${count}`)
+    await new Promise(resolve => setTimeout(resolve, 500)) // wait to not overload the API
+  }
+  
+  // Commence scraping!
+  await scrape()
+  while (offset < total) await scrape()
+  console.log(`🎉 Done, scraped ${results.length} items, saving...`)
+  
+  // Save as array
+  fs.writeFileSync(`resultsArray.json`, JSON.stringify(results, null, 2))
+  console.log(`💾 Saved as resultsArray.json`)
+  
+  // Save as object indexed by "name_for_model"
+  const resultsIndexedByName = results.reduce((acc, item) => {
+    const slug = itemToSlug(item)
+    if (acc[slug]) console.log(`🧐 Suspicious, duplicate name_for_model: ${slug}`)
+    acc[slug] = item
+    return acc
+  }, {})
+  fs.writeFileSync(`resultsIndexed.json`, JSON.stringify(resultsIndexedByName, null, 2))
+  console.log(`💾 Saved as resultsIndexed.json`)
+  
+  // Save as file per plugin
+  for (const item of results) {
+    const slug = itemToSlug(item)
+    fs.writeFileSync(`results/${slug}.json`, JSON.stringify(item, null, 2))
+  }
+  console.log(`💾 Saved as results/*.json`)
 }
 
-// Commence scraping!
-await scrape()
-while (offset < total) await scrape()
-console.log(`🎉 Done, scraped ${results.length} items, saving...`)
-
-// Save as array
-fs.writeFileSync(`resultsArray.json`, JSON.stringify(results, null, 2))
-console.log(`💾 Saved as resultsArray.json`)
-
-// Save as object indexed by "name_for_model"
-const resultsIndexedByName = results.reduce((acc, item) => {
-  const slug = itemToSlug(item)
-  if (acc[slug]) console.log(`🧐 Suspicious, duplicate name_for_model: ${slug}`)
-  acc[slug] = item
-  return acc
-}, {})
-fs.writeFileSync(`resultsIndexed.json`, JSON.stringify(resultsIndexedByName, null, 2))
-console.log(`💾 Saved as resultsIndexed.json`)
-
-// Save as file per plugin
-for (const item of results) {
-  const slug = itemToSlug(item)
-  fs.writeFileSync(`results/${slug}.json`, JSON.stringify(item, null, 2))
+async function downloadPluginApiManifests({ onlyMissing = false }) {
+  // when debugging, we may not run the whole scrape, so potentially use the existing results from disk
+  if (!results.length) results = JSON.parse(fs.readFileSync(`resultsArray.json`, `utf8`))
+  for (const item of results) {
+    const slug = itemToSlug(item)
+    if (onlyMissing && fileStartingWithExists(`results`, `${slug}.api.`)) continue
+    console.log(`📥 Downloading ${itemToSlug(item)}.api.*`)
+    try {
+      await downloadApiManifest(item)
+      console.log(`   ✅`)
+    } catch (e) {
+      console.log(`   ❌ ${e.message}`)
+      logProblemToFile(`Failed to download API manifest for ${item.domain}/${item.manifest.name_for_model}:\n${e.message}`)
+    }
+  }
 }
-console.log(`💾 Saved as results/*.json`)
+
+// Main flow – for easier debugging divide into two independent steps
+await downloadPluginManifests()
+await downloadPluginApiManifests({ onlyMissing: false }) // set onlyMissing for easier debugging
 
 // Helpers
 // ===
@@ -64,4 +88,40 @@ function itemToSlug (item) {
   const { manifest, domain } = item
   const { name_for_model } = manifest
   return domain.replace(/\./g, `_`) + `__` + name_for_model
+}
+
+function fileStartingWithExists (dir = `results`, needle) {
+  return fs.readdirSync(dir).some(filename => filename.startsWith(needle))
+}
+
+async function downloadApiManifest(item) {
+  const slug = itemToSlug(item)
+  const { manifest } = item
+  const { api } = manifest
+  
+  let ext // url may not end with file extension, fallback to response content-type 
+  const  { type, url } = api
+  if (url.endsWith(`.json`)) ext = `json`
+  else if (url.endsWith(`.yaml`)) ext = `yaml`
+  const res = await fetch(url)
+  const contentType = res.headers.get(`content-type`)
+  if (!ext) {
+    if (contentType.includes(`json`)) ext = `json`
+    else if (contentType.includes(`yaml`)) ext = `yaml`
+    else {
+      console.log(`🧐 Suspicious, unknown content-type: ${contentType} for ${url}`)
+      ext = `txt`
+    }
+  }
+  const filename = `${slug}.api.${ext}`
+  const text = await res.text()
+  let formattedText
+  if (ext === `json`) formattedText = JSON.stringify(JSON.parse(text), null, 2)
+  else if (ext === `yaml`) formattedText = text // TODO: normalize yaml?
+  fs.writeFileSync(`results/${filename}`, formattedText)
+}
+
+function logProblemToFile(problem) {
+  if (!fs.existsSync(`problems.txt`)) fs.writeFileSync(`problems.txt`, ``)
+  fs.appendFileSync(`problems.txt`, `${new Date().toISOString()}: ${problem}\n`)
 }
